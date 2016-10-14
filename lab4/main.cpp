41ca593b77a2f0afe7b008a66a389eff384fa63f
wsyccc <wsyccc@hotmail.com>
2016-10-14 00:42:20 -0700

    vector

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 
 using namespace std;
 
@@ -26,12 +27,34 @@ int kth_selection(const vector<int> &v, size_t k) {
     else if(right.size() > k)
         return kth_selection(right, k);
 }
+
 vector<string> subsets(size_t n){
-    vector<string> v(n);
+    string subet;
+    if (n < 0){
+        cout << subet <<endl;
+        vector<string> v;
+        v.push_back(subet);
+        return v;
+    }
+    cout << subet << endl;
+    subet.at(n) = 0;
+    subsets(n-1);
+    subet.at(n) = 1;
+    subsets(n-1);
+
 }
 
+
 int main() {
-    vector<int> v= {2,3,1,0,4,2,1,3};
-    cout << kth_selection(v, 3) << endl;
+    //vector<int> v= {2,3,1,0,4,2,1,3};
+    //cout << kth_selection(v, 3) << endl;
+//    vector<string> v = subsets(4);
+//    for (auto it = v.begin(); it != v.end(); ++it) {
+//        cout << *it << endl;
+//    }
+    string test(5,2);
+    //test.at(2) = 1;
+    cout << test << endl;
+
     return 0;
 }
